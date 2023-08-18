1ba666306969ecd96a7758fd36b24a20a5f6c267
Valisoa <k.ranjohanison@etechconsulting-mg.com>
2023-08-18 16:00:36 +0300

    very long commits

---
+++ koly.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mode: "jit",
+    purge: ["./hubspot-practicum/**/*.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
